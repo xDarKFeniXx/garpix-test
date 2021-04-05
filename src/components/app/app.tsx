122e1ad440e809450ee9c23cb106faf07515867e
xDarKFeniXx <dark.fenix.tula@gmail.com>
2021-04-05 17:46:54 +0300

    add error-boundary.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,23 +2,26 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 import {PersistGate} from "redux-persist/integration/react";
-import { useRoutes } from '../../routes';
+import {useRoutes} from '../../routes';
 import configureStore from '../../store/store';
 import {LayoutApp} from '../layout/layout';
 import 'antd/dist/antd.css'
+import ErrorBoundary from '../error-boundary/error-boundary';
 
-const {store, persistor}=configureStore()
+const {store, persistor} = configureStore()
 
 function App() {
-    const routes=useRoutes()
+    const routes = useRoutes()
     return (
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-            <BrowserRouter>
-                <LayoutApp>
-                    {routes}
-                </LayoutApp>
-            </BrowserRouter>
+                <ErrorBoundary>
+                    <BrowserRouter>
+                        <LayoutApp>
+                            {routes}
+                        </LayoutApp>
+                    </BrowserRouter>
+                </ErrorBoundary>
             </PersistGate>
         </Provider>
     );
